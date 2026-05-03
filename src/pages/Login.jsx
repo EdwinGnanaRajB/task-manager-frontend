0b9b02982509f48e0b230ae5fdc960c49d61abbc
EdwinGnanaRajB <edwingnanarajb@gmail.com>
2026-05-03 17:42:59 +0530

    fix useNavigate

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import { motion } from "framer-motion";
-import { Link } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
 import API from "../services/api";
 
 function Login() {
